6a11e540aeb1996011286c0d3e63997683a5beb6
Amanda Leaders <alsleaders@gmail.com>
2019-08-08 22:02:00 -0400

    box and border work

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,8 +1,40 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class HelloWorld extends Component {
+  state = {
+    boxColor: '',
+    borderColor: ''
+  }
+
+  componentDidMount() {
+    axios.get('https://api.noopschallenge.com/hexbot?count=2').then(resp => {
+      console.log(resp.data)
+      console.log(resp.data.colors)
+      console.log(resp.data.colors[0].value)
+      this.setState({
+        boxColor: resp.data.colors[0].value,
+        borderColor: resp.data.colors[1].value
+      })
+    })
+  }
+
   render() {
-    return <h1>Hello, World!</h1>
+    return (
+      <>
+        <section
+          className="hex-color"
+          style={{ background: this.state.borderColor }}
+        >
+          <div className="boxShape" style={{ background: this.state.boxColor }}>
+            <p>
+              This is {this.state.boxColor} <br /> with a{' '}
+              {this.state.borderColor} border
+            </p>
+          </div>
+        </section>
+      </>
+    )
   }
 }
 
